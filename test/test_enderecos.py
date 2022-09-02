0a781cb365f3f3525bce7accc48870b026c6ebc7
gustavolp1 <glpachecocsa@gmail.com>
2022-09-02 13:00:33 -0300

    ex2 pronto

--- test/test_enderecos.py
+++ test/test_enderecos.py
@@ -4,23 +4,28 @@ import requests
 from classes.Endereco import Endereco
 
 #requisito 1.1: FEITO
-@pytest.mark.teste_generico
+@pytest.mark.teste_1_1
 def test_cep_e_numero():
     endereco = Endereco('08320330', 430)
     assert endereco.cep == '08320330'
     assert endereco.numero == 430
 
-#requisito 1.2: FEITO
-@pytest.mark.teste_generico
+#requisito 2.1: FEITO
+@pytest.mark.teste_2_1
 def test_cep_int_ou_string():
     endereco = Endereco(8320330, 430)
     assert endereco.cep == '08320330'
 
-#requisito 2.1: TODO
-
-#requisito 2.2: TODO
+#requisito 2.2: FEITO
+@pytest.mark.teste_2_2
+def test_return_falso_cep_inverificavel():
+    endereco = Endereco.consultar_cep('')
+    if endereco == {'erro': True}:
+        endereco = False
+    assert endereco == False
 
 #requisito 2.3 FEITO
+@pytest.mark.teste_2_3
 def test_consulta_cep_sem_conexao():
     with pytest.raises(requests.exceptions.ConnectionError):
         Endereco.consultar_cep('08320330')
